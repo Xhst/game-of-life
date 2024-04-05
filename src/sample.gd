e7dc18daa3ef9e4585378b8659ace2f3b449da65
Mattia <82611659+Xhst@users.noreply.github.com>
2024-04-05 22:36:57 +0200

    docs: sample

--- src/sample.gd
+++ src/sample.gd
@@ -1,4 +1,8 @@
 class_name Sample
+"""
+The Sample class provides functionality for working with sample textures.
+"""
+
 enum Name {
 	NONE,
 	GLIDER,
@@ -10,13 +14,25 @@ enum Name {
 }
 
 static func texture_from_name(name: Sample.Name) -> Texture2D:
+	"""
+	Returns the texture corresponding to the given sample name.
+
+	Args:
+		name (Sample.Name): The name of the sample.
+
+	Returns:
+		Texture2D: The texture corresponding to the given sample name.
+	"""
+	# Check if the name is NONE
 	if name == Sample.Name.NONE:
 		return null
-		
-	var sample_dir = "res://images/samples/"
+
+	# Define the directory and file name for the sample texture
+	var sample_dir = 'res://images/samples/'
 	var sample_name = str(Name.keys()[name]).to_lower()
 	var path = sample_dir + sample_name + '.png'
 
+	# Load the texture from the specified path
 	var texture: Texture2D = load(path)
-	
+
 	return texture
